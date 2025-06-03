71926d1defb5033aa1320afd7f7bb3aee19b9da9
Pritesh Nikam <priteshnikam8121@gmail.com>
2025-06-03 22:46:12 +0530

    handle btn condition for timer screen

--- app/src/main/java/com/developersphere/clock/presentation/screens/timer_screen/TimerViewModel.kt
+++ app/src/main/java/com/developersphere/clock/presentation/screens/timer_screen/TimerViewModel.kt
@@ -34,6 +34,9 @@ class TimerViewModel @Inject constructor() : ViewModel() {
         _timerSecond.value = value
     }
 
+    private val _showTimer = MutableStateFlow(false)
+    var showTimer: StateFlow<Boolean> = _showTimer
+
     private val _isRunning = MutableStateFlow(false)
     var isRunning: StateFlow<Boolean> = _isRunning
 
@@ -43,13 +46,14 @@ class TimerViewModel @Inject constructor() : ViewModel() {
     private var _job: Job? = null
 
     fun startTimer() {
+        _showTimer.value = true
         _isRunning.value = true
         _remainingMilliSeconds.value =  (_timerHour.value * 3600) + (_timerMinute.value * 60) + _timerSecond.value
         _job = viewModelScope.launch {
             while (isActive) {
                 if(remainingMilliSeconds.value == 0)
                 {
-                   _isRunning.value = false
+                   _showTimer.value = false
                     break
                 }
                 _remainingMilliSeconds.value -= 1
@@ -60,10 +64,12 @@ class TimerViewModel @Inject constructor() : ViewModel() {
 
     fun stopTimer() {
         _job?.cancel()
+        _isRunning.value = false
     }
 
     fun resetTimer() {
         _job?.cancel()
+        _showTimer.value = false
         _isRunning.value = false
         _timerHour.value = 0
         _timerMinute.value = 15
